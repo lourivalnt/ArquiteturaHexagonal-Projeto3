214293f0dd173da030a74349c618b28b51c1ae0a
Lourival Linard <l.linard@gmail.com>
2021-02-03 09:06:42 -0300

    Projeto 3 - Aula 10

--- src/test/java/teste/integracao/TesteContaRepositorio.java
+++ src/test/java/teste/integracao/TesteContaRepositorio.java
@@ -18,13 +18,77 @@ import static org.junit.jupiter.api.Assertions.*;
 @ContextConfiguration(classes = Config.class)
 public class TesteContaRepositorio {
 
+    @Inject
+    ContaRepositorio rep;
+
+    // negativos get
+
     @Test
     @DisplayName("pesquisa conta com número nulo")
     void teste1() {
         try {
-          System.out.println("teste ola");
+            var conta = rep.get(null);
+            assertTrue(conta == null, "Conta deve ser nula");
         } catch (NegocioException e) {
             fail("Deve carregar uma conta nula.");
+        }
+    }
+
+    @Test
+    @DisplayName("pesquisa conta com número inexistente")
+    void teste2() {
+        try {
+            var conta = rep.get(8547);
+            assertTrue(conta == null, "Conta deve ser nula");
+        } catch (NegocioException e) {
+            fail("Deve carregar uma conta nula.");
+        }
+    }
+
+    // positivo get
+
+    @Test
+    @DisplayName("pesquisa conta com número existente")
+    void teste3() {
+        try {
+            var conta = rep.get(50);
+            assertTrue(conta != null, "Conta deve estar preenchida");
+            System.out.println(conta);
+        } catch (NegocioException e) {
+            fail("Deve carregar uma conta.");
+        }
+    }
+
+    // negativos alterar
+
+    @Test
+    @DisplayName("alterar conta como nulo")
+    void teste4() {
+        try {
+            rep.alterar(null);
+            fail("Não deve alterar conta nula");
+        } catch (NegocioException e) {
+            assertEquals(e.getMessage(), "Conta é obrigatório.");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // positivo alterar
+
+    @Test
+    @DisplayName("alterar conta com sucesso")
+    void teste5() {
+        try {
+            var c1 = rep.get(50);
+            c1.setSaldo(new BigDecimal("1.00"));
+            c1.setCorrentista("Alterado");
+            rep.alterar(c1);
+
+            var c2 = rep.get(50);
+            assertEquals(c1.getSaldo(), c2.getSaldo(), "Deve bater o saldo");
+            assertEquals(c1.getCorrentista(), c2.getCorrentista(), "Deve bater o correntista");
+        } catch (NegocioException e) {
+            fail("Deve alterar conta ");
         }
     }
 }
