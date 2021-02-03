eba8e1b33bde0a2d181cfb0dff06a34eb4e0849e
Lourival Linard <l.linard@gmail.com>
2021-02-03 00:47:43 -0300

    Projeto 3 - Aula 7

--- src/main/java/conta/servicos/respositorio/ContaRepositorioImp.java
+++ src/main/java/conta/servicos/respositorio/ContaRepositorioImp.java
@@ -5,6 +5,7 @@ import conta.sistema.dominio.modelo.NegocioException;
 import conta.sistema.porta.ContaRepositorio;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -44,8 +45,19 @@ public class ContaRepositorioImp implements ContaRepositorio {
         }
     }
 
+    @Transactional
     @Override
     public void alterar(Conta conta) {
-
+        if (isNull(conta)) {
+            throw new NegocioException("Conta é obrigatório.");
+        }
+        var sql = "update conta set saldo=?, correntista=? where numero=?";
+        var pm = new Object[]{conta.getSaldo(), conta.getCorrentista(), conta.getNumero()};
+        try {
+            jdbc.update(sql, pm);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new NegocioException(ERRO);
+        }
     }
 }
